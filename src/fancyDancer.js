21f772b3325191dd0d92bad20bf879a8facb03ce
Hack Reactor Students <danny.nelson@hackreactor.com>
2013-11-19 12:36:15 -0800

    Dancers react to mouseenter

--- src/fancyDancer.js
+++ src/fancyDancer.js
@@ -1,5 +1,6 @@
 var FancyDancer = function(top, left, timeBetweenSteps) {
   this.oldStep = Dancer.prototype.step;
+  this.colored = true;
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass("fancy");
 };
@@ -8,4 +9,15 @@ FancyDancer.prototype = Object.create(Dancer.prototype);
 FancyDancer.prototype.constructor = FancyDancer;
 FancyDancer.prototype.step = function() {
   this.oldStep();
+  this.changeColor();
+};
+FancyDancer.prototype.changeColor = function() {
+  debugger;
+  if (this.colored) {
+    this.$node.css('border-color', 'red');
+    this.colored = false;
+  } else {
+    this.$node.css('border-color', 'yellow');
+    this.colored = true;
+  }
 };
